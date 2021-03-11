88e4aeb0ff38afe80b7c96198bd6a5c580e3ba6d
Edward Nygma <enygma@yahoo.com>
2021-03-11 15:59:09 -0600

    it shoul work
    Signed-off-by: Edward Nygma <enygma@yahoo.com>

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf googletest-release-1.10.0
+wget -qO- https://github.com/google/googletest/archive/release-1.10.0.tar.gz | tar xvzf -
+cd googletest-release-1.10.0
+export GTEST=$(pwd)
+cmake .
+make
+cd -
+cmake .
+make clean
+make
